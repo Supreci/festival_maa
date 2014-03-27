2648ea0c010512344a276b1337ecd8f6cf8bea82
Supreci <alexis.hamonic@orange.fr>
2014-03-27 16:41:48 +0100

    Connexion+Classes

---
+++ src/festivalmaa/FestivalMAA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package festivalmaa;
+
+/**
+ *
+ * @author etudsio
+ */
+public class FestivalMAA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
